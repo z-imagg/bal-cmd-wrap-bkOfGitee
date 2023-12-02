91c7fe3ea9a38b3e234c351d4596a3e6de85958b
prgrmz01@163.com <prgrmz01@163.com>
2023-12-02 11:46:06 +0800

    命名规范

--- cmd_parser.py
+++ cmd_parser.py
@@ -1,19 +1,19 @@
 from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList,OneOrMore
 
-def parser():
+def create_singleCmdParser():
     T=alphanums + '_' + '-' + '.'
     prog=Word(alphas,alphanums+'_')
     argName1=Word('-',T)
     argVal1=quotedString  | Word(T)
-    param = prog +  OneOrMore(argName1 + argVal1)
-    return param
+    singleCmd = prog +  OneOrMore(argName1 + argVal1)
+    return singleCmd
 
 
 
 
-cmd = 'gcc -c "My User.c" -o "User.o"'
-cmd_parser=parser()
-result = cmd_parser.parseString(cmd)
+singleCmd = 'gcc -c "My User.c" -o "User.o"'
+singleCmdParser=create_singleCmdParser()
+parse_result = singleCmdParser.parseString(singleCmd)
 
-print(f"{result.asList()}")
+print(f"{parse_result.asList()}")
 #['gcc', '-c', '"My User.c"', '-o', '"User.o"']
