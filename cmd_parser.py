30865b0dcb420746b96ab06fe8703159d51cb7e5
prgrmz01@163.com <prgrmz01@163.com>
2023-12-02 12:16:46 +0800

    失败：参数值可选，win10下调试报错

--- cmd_parser.py
+++ cmd_parser.py
@@ -1,4 +1,4 @@
-from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList,OneOrMore
+from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList,OneOrMore,CaselessLiteral
 
 def create_singleCmdParser():
     T=alphanums + '_-./'
@@ -6,7 +6,7 @@ def create_singleCmdParser():
     argName1=Word('-',T)
     argName2=Word('--',T)
     argVal1=quotedString  | Word(T)
-    singleCmd = prog +  OneOrMore( (argName1|argName2) + argVal1  ) + Optional(';')
+    singleCmd = prog +  OneOrMore( (argName1|argName2) + Optional(argVal1)  ) + Optional(';')
     return singleCmd
 
 
@@ -15,12 +15,14 @@ def create_singleCmdParser():
 singleCmdParser=create_singleCmdParser()
 
 cmdLs = [
-'gcc --fXXX yes -c "My User.c" -o "User.o"',
-'g++ -c ./common/Util.cpp -o ../build/Util.obj'
+'gcc --fXXX -c "My User.c" -o "User.o"',
+'g++ --g --c ./common/Util.cpp -o ../build/Util.obj'
 ]
 for cmdK in cmdLs:
-    cmdKResult = singleCmdParser.parseString(cmdK)
+    cmdKResult = singleCmdParser.parse_string(cmdK)
+    # print(cmdKResult.dump())
     print(f"{cmdKResult.asList()}")
+    break
 
 # ['gcc', '--fXXX', 'yes', '-c', '"My User.c"', '-o', '"User.o"']
 # ['g++', '-c', './common/Util.cpp', '-o', '../build/Util.obj']
