929818b4b32441c2fcba19b7b89074aad3a5c8dd
prgrmz01@163.com <prgrmz01@163.com>
2023-12-02 11:35:23 +0800

    pyparsing正常解析 简单命令

--- cmd_parser.py
+++ cmd_parser.py
@@ -1,28 +1,24 @@
-from pyparsing import Word, alphas, alphanums, QuotedString, Optional, Group, delimitedList
+from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList
 
-def parse_command(command):
-    # 定义语法规则
+def parser():
     T=alphanums + '_' + '-'
-    ID=Word(alphas,T)
-    param_name_1=Word('-',T)
-    param_name_2=Word('--',T)
-    param_name = ID | param_name_1 | param_name_2
-    param_value = QuotedString(quoteChar='"') | QuotedString(quoteChar="'") | Word(T)
-    param = Group(param_name + Optional("=") + Optional(param_value) ) | param_value
+    prog=Word(alphas,alphanums+'_')
+    argName1=Word('-',T)
+    argVal1=quotedString  | Word(T)
+    param = prog +  argName1 + argVal1
     command_parser = delimitedList(param)
+    return command_parser
 
-    # 解析命令
-    parsed_params = command_parser.parseString(command)
 
-    # 输出参数名和值
-    for param in parsed_params:
-        param_name = param[0] if len(param) > 0 else ""
-        param_value = param[1] if len(param) > 1 else ""
-        print(f"参数名: {param_name}, 值: {param_value}")
+
 
 # 示例命令
-# example_command = 'command -param1 value1 --p2 "Val2"  param5'
-example_command = 'command --p2 "Val2"  param5'
+# cmd = 'command -param1 value1 --p2 "Val2"  param5'
+# cmd = 'command --p2 "Val2"  param5'
+cmd = 'command -p2 Val2'
+# 解析命令
+command_parser=parser()
+prog,argName1,argVal1 = command_parser.parseString(cmd)
 
-# 调用函数解析命令
-parse_command(example_command)
+print(f"prog:{prog},参数名: {argName1}, 值: {argVal1}")
+# prog:command,参数名: -p2, 值: Val2
