37bb458b5fc08927a43e17ffb20bbee63767bcf7
prgrmz01@163.com <prgrmz01@163.com>
2023-12-02 11:41:16 +0800

    pyparsing正常解析 简单命令 多参数

--- cmd_parser.py
+++ cmd_parser.py
@@ -1,24 +1,19 @@
-from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList
+from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList,OneOrMore
 
 def parser():
-    T=alphanums + '_' + '-'
+    T=alphanums + '_' + '-' + '.'
     prog=Word(alphas,alphanums+'_')
     argName1=Word('-',T)
     argVal1=quotedString  | Word(T)
-    param = prog +  argName1 + argVal1
-    command_parser = delimitedList(param)
-    return command_parser
+    param = prog +  OneOrMore(argName1 + argVal1)
+    return param
 
 
 
 
-# 示例命令
-# cmd = 'command -param1 value1 --p2 "Val2"  param5'
-# cmd = 'command --p2 "Val2"  param5'
-cmd = 'command -p2 Val2'
-# 解析命令
-command_parser=parser()
-prog,argName1,argVal1 = command_parser.parseString(cmd)
+cmd = 'gcc -c User.c -o User.o'
+cmd_parser=parser()
+result = cmd_parser.parseString(cmd)
 
-print(f"prog:{prog},参数名: {argName1}, 值: {argVal1}")
-# prog:command,参数名: -p2, 值: Val2
+print(f"{result.asList()}")
+# ['gcc', '-c', 'User.c', '-o', 'User.o']
