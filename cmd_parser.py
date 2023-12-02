3071695bacfb8c9645792fa6c7e286cbd9dccd59
prgrmz01@163.com <prgrmz01@163.com>
2023-12-02 12:01:43 +0800

    增加 --参数名 、去掉不必要空格

--- cmd_parser.py
+++ cmd_parser.py
@@ -2,10 +2,11 @@ from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, de
 
 def create_singleCmdParser():
     T=alphanums + '_-./'
-    prog=Word(alphas,alphanums+'_ +')
+    prog=Word(alphas,alphanums+'_+')
     argName1=Word('-',T)
+    argName2=Word('--',T)
     argVal1=quotedString  | Word(T)
-    singleCmd = prog +  OneOrMore(argName1 + argVal1) + Optional(';')
+    singleCmd = prog +  OneOrMore( (argName1|argName2) + argVal1  ) + Optional(';')
     return singleCmd
 
 
@@ -14,12 +15,12 @@ def create_singleCmdParser():
 singleCmdParser=create_singleCmdParser()
 
 cmdLs = [
-'gcc -c "My User.c" -o "User.o"',
+'gcc --fXXX yes -c "My User.c" -o "User.o"',
 'g++ -c ./common/Util.cpp -o ../build/Util.obj'
 ]
 for cmdK in cmdLs:
     cmdKResult = singleCmdParser.parseString(cmdK)
     print(f"{cmdKResult.asList()}")
 
-# ['gcc ', '-c', '"My User.c"', '-o', '"User.o"']
-# ['g++ ', '-c', './common/Util.cpp', '-o', '../build/Util.obj']
+# ['gcc', '--fXXX', 'yes', '-c', '"My User.c"', '-o', '"User.o"']
+# ['g++', '-c', './common/Util.cpp', '-o', '../build/Util.obj']
