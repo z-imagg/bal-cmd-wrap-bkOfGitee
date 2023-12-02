7f7e640ebe88c5812edf8eb0094e551950c5b547
prgrmz01@163.com <prgrmz01@163.com>
2023-12-02 11:52:22 +0800

    pyparsing 命令中含+、参数中含/、结尾分号可选，循环解析

--- cmd_parser.py
+++ cmd_parser.py
@@ -1,19 +1,25 @@
 from pyparsing import Word, alphas, alphanums, quotedString, Optional, Group, delimitedList,OneOrMore
 
 def create_singleCmdParser():
-    T=alphanums + '_' + '-' + '.'
-    prog=Word(alphas,alphanums+'_')
+    T=alphanums + '_' + '-' + '.' + '/'
+    prog=Word(alphas,alphanums+'_ +')
     argName1=Word('-',T)
     argVal1=quotedString  | Word(T)
-    singleCmd = prog +  OneOrMore(argName1 + argVal1)
+    singleCmd = prog +  OneOrMore(argName1 + argVal1) + Optional(';')
     return singleCmd
 
 
 
 
-singleCmd = 'gcc -c "My User.c" -o "User.o"'
 singleCmdParser=create_singleCmdParser()
-parse_result = singleCmdParser.parseString(singleCmd)
 
-print(f"{parse_result.asList()}")
-#['gcc', '-c', '"My User.c"', '-o', '"User.o"']
+cmdLs = [
+'gcc -c "My User.c" -o "User.o"',
+'g++ -c ./common/Util.cpp -o ../build/Util.obj'
+]
+for cmdK in cmdLs:
+    cmdKResult = singleCmdParser.parseString(cmdK)
+    print(f"{cmdKResult.asList()}")
+
+# ['gcc ', '-c', '"My User.c"', '-o', '"User.o"']
+# ['g++ ', '-c', './common/Util.cpp', '-o', '../build/Util.obj']
