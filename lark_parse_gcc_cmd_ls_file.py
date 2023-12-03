ba93314ced0feb3178cbd44ab35b6b75550c6041
prgrmz01@163.com <prgrmz01@163.com>
2023-12-03 17:07:32 +0800

    不用treeK.pretty()，其占用行数太多

--- lark_parse_gcc_cmd_ls_file.py
+++ lark_parse_gcc_cmd_ls_file.py
@@ -21,12 +21,12 @@ parser = Lark.open( 'linux_cmd.lark', rel_to=__file__, parser="earley")
 
 for k,lnk in enumerate(lns) :
     treeK = parser.parse(lnk)
-    # print(tree) ; print( treeK.pretty() )
+    # print(treeK) ; print( treeK.pretty() )
 
     #获取 结果树 中的 src_file 非终结符 节点 的 值
     transformer = MyTransformer()
     transformer.transform(treeK)
     src_file_val: str = transformer.__get_src_file_val__("")
 
-    msg:str=f"lark文法 解析 文件【{inFilePath}】的第{k}行  【{lnk}】 ,解析结果为: 【{treeK.pretty()}】，啰嗦一下 现在是 第{k}行, 命令中的源文件为:【{src_file_val}】"
+    msg:str=f"lark文法 解析 文件【{inFilePath}】的第{k}行  【{lnk}】 ,解析结果为: 【{treeK }】，啰嗦一下 现在是 第{k}行, 命令中的源文件为:【{src_file_val}】"
     print(msg)
