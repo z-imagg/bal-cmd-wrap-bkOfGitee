6ce6638e84f0c4b6545a92a7cba1aa363842ea8b
prgrmz01@163.com <prgrmz01@163.com>
2023-12-03 13:49:38 +0800

    lark又对了

--- linux_cmd_lark.py
+++ linux_cmd_lark.py
@@ -2,11 +2,12 @@ from lark import Lark
 from lark import common
 # from lark.common import ESCAPED_STRING
 
-input_string = 'gcc   -nostdinc  -isystem '
+input_string = 'gcc -Wp,-MD,arch/x86/mm/.pgtable.o.d -nostdinc -isystem'
+# input_string = 'gcc   -nostdinc  -isystem '
 # input_string = 'gcc   -c -o xxx'
 
-
-parser = Lark.open( 'linux_cmd.lark', rel_to=__file__, parser="lalr")
+# lark.open的参数parser 取值范围 为 ('earley', 'lalr', 'cyk', None)
+parser = Lark.open( 'linux_cmd.lark', rel_to=__file__, parser="earley") # 正常运行: earley 、 lalr ;  报错: cyk
 
 # parser = Lark(grammar, start='singlcmd')
 tree = parser.parse(input_string)
@@ -18,6 +19,11 @@ start
   single_cmd
     program	gcc
     kv_ls
+      kv
+        kv2
+          key	-Wp
+          kv_sep2
+          val_2	-MD,arch/x86/mm/.pgtable.o.d
       kv
         key	-nostdinc
       kv
