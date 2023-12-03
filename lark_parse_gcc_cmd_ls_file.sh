64b4bcc75331832f848e4f124d19b13f5e2d57be
prgrmz01@163.com <prgrmz01@163.com>
2023-12-03 19:28:18 +0800

    gcc开头的行

--- lark_parse_gcc_cmd_ls_file.sh
+++ lark_parse_gcc_cmd_ls_file.sh
@@ -5,4 +5,5 @@
 source /app/miniconda3/bin/activate && \
 pip install lark && \
 cd /crk/clang-wrap/ && \
+grep -E "^[ ]*gcc" make_clean_then_make_V\=1.txt > gcc_cmd_ls.txt && \
 python lark_parse_gcc_cmd_ls_file.py 2>&1 | tee lark_parse_gcc_cmd_ls_file.log
