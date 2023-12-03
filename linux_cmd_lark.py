db0097948e7ba9d3ef7aaf890291941443873d38
prgrmz01@163.com <prgrmz01@163.com>
2023-12-03 13:24:29 +0800

    lark又对了

--- linux_cmd_lark.py
+++ linux_cmd_lark.py
@@ -2,8 +2,8 @@ from lark import Lark
 from lark import common
 # from lark.common import ESCAPED_STRING
 
-# input_string = 'gcc   -nostdinc -isystem /usr/lib/gcc/i686-linux-gnu/4.4.7/include -D__KERNEL__ -Iinclude -I/crk/bochs/linux2.6-run_at_bochs/linux-2.6.27.15/arch/x86/include -include include/linux/autoconf.h -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -O2 -m32 -msoft-float -mregparm=3 -freg-struct-return -mpreferred-stack-boundary=2 -march=i686 -mtune=generic -ffreestanding -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -Iinclude/asm-x86/mach-default -Wframe-larger-than=1024 -fno-stack-protector -fno-omit-frame-pointer -fno-optimize-sibling-calls -g -pg -Wdeclaration-after-statement -Wno-pointer-sign -D"KBUILD_STR(s)=#s" -D"KBUILD_BASENAME=KBUILD_STR(pgtable)" -D"KBUILD_MODNAME=KBUILD_STR(pgtable)" -c -o arch/x86/mm/.tmp_pgtable.o arch/x86/mm/pgtable.c'
-input_string = 'gcc   -c -o xxx'
+input_string = 'gcc   -nostdinc  -isystem '
+# input_string = 'gcc   -c -o xxx'
 
 
 parser = Lark.open( 'linux_cmd.lark', rel_to=__file__, parser="lalr")
@@ -19,8 +19,7 @@ start
     program	gcc
     kv_ls
       kv
-        key	c
+        key	-nostdinc
       kv
-        key	o
-        val	xxx
+        key	-isystem
 """
