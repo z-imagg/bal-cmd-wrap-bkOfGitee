6cfbe3ad3fda1897fc2bd3a1276b0cc5a4077089
prgrmz01@163.com <prgrmz01@163.com>
2023-12-03 15:15:01 +0800

    fix

--- str_util.py
+++ str_util.py
@@ -1,18 +1,18 @@
-def removeEndLineFeed(lnk:str)->str:
-    assert lnk is not None
-    lnLen:int=len(lnk)
-    if(lnLen==0): return lnk
+def removeEndLineFeed(lnK:str)->str:
+    assert lnK is not None
+    lnLen:int=len(lnK)
+    if(lnLen==0): return lnK
 
     #行末尾 微软windows换行符\r\n 删除
-    if lnLen>=2 and lnk[-2:] == "\r\n":
+    if lnLen>=2 and lnK[-2:] == "\r\n":
         return lnK[0:-2]
 
     #行末尾 linux换行符\n 删除
-    if lnLen >= 1 and lnk[-1:] == "\n":
+    if lnLen >= 1 and lnK[-1:] == "\n":
         return lnK[0:-1]
 
     #行末尾 macOS换行符\r 删除
-    if lnLen >= 1 and lnk[-1:] == "\r":
+    if lnLen >= 1 and lnK[-1:] == "\r":
         return lnK[0:-1]
 
-    return lnk
+    return lnK
