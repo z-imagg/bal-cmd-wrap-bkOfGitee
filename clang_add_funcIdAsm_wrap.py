b5bff23ddd8ab09e9d074ae9f1d90584fb5afcb0
prgrmz01@163.com <prgrmz01@163.com>
2023-12-04 22:51:37 +0800

    远端主机中执行clang插件  开发中

--- clang_add_funcIdAsm_wrap.py
+++ clang_add_funcIdAsm_wrap.py
@@ -1,6 +1,47 @@
 from typing import List
 
+#pip install paramiko
+
+import paramiko
+from paramiko import SSHClient
 from lark_parser.file_at_cmd import FileAtCmd
-def clangAddFuncIdAsmWrap(fileAtCmd:FileAtCmd):
+def __get_ubuntu22x64HostSshClient__()->SSHClient:
+    # 创建 SSH 客户端
+    ubt22:SSHClient = SSHClient()
+
+    # 自动添加远程主机的密钥
+    ubt22.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # 连接远程主机
+    # F:\crk\bochs\linux2.6-run_at_bochs\readme.md
+    """
+# 各主机访问端口备忘
+- win10Ssh: 192.168.1.13:3022; (__*)
+- ubuntu14x32: 192.168.1.4:3022; (TPLINK_*)
+- ubuntu22x64: 192.168.1.4:2122; (TPLINK_*)
+    """
+    passwd_z = input("请输入主机ubuntu22x64的用户z的密码:")
+    ubt22.connect('ubuntu22x64', port=2122, username='z', password=passwd_z)
+
+    return ubt22
+
+def clangAddFuncIdAsmWrap(fileAtGccCmd:FileAtCmd):
     #TODO : 调用远端主机ubuntu22x64上的clang-add-funcIdAsm插件修改本地ubuntu14x32上的源文件 , 源文件路径 及 头文件目录列表为 在 入参对象 fileAtCmd 中
-    pass
+
+    ubt22:SSHClient=__get_ubuntu22x64HostSshClient__()
+
+    #  组装 clang 插件命令
+    clang_plugin_so="/crk/clang-add-funcIdAsm/build/lib/libCTk.so"
+    as_clang_cmd_part:str=fileAtGccCmd.__as_clang_cmd_part__()
+    clang_plugin_cmd:str=f"/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang  -Xclang   -load -Xclang {clang_plugin_so}  -Xclang   -add-plugin -Xclang  CTk   {as_clang_cmd_part}"
+    #  clang 插件命令
+    stdin, stdout, stderr = ubt22.exec_command(clang_plugin_cmd)
+
+    # 获取命令输出
+    output = stdout.read().decode('utf-8')
+
+    # 打印输出结果
+    print(output)
+
+    # 关闭 SSH 连接
+    ubt22.close()
--- lark_parser/file_at_cmd.py
+++ lark_parser/file_at_cmd.py
@@ -11,5 +11,10 @@ class FileAtCmd:
 
     def __str__(self):
         srcFile='' if self.src_file is None else self.src_file
-        msg= f"src_file:【{srcFile}】, 【-isystem】:【{self.isystem_val_ls}】, 【-I】:【{self.inc_val_ls}】, 【-I 】:【{self.sep_inc_val_ls}】, 【-include】:【{self.sep_include_val_ls}】"
+        msg= f"src_file:【{srcFile}】, 【-isystem 】:【{self.isystem_val_ls}】, 【-I】:【{self.inc_val_ls}】, 【-I 】:【{self.sep_inc_val_ls}】, 【-include】:【{self.sep_include_val_ls}】"
         return msg
+
+    def __as_clang_cmd_part__(self)->str:
+        ubt14x86root:str='/ubt14x86root/'
+        as_clang_cmd_part= f" {'-isystem '.join(self.isystem_val_ls)} {'-I'.join(self.inc_val_ls)}  {'-I '.join(self.sep_inc_val_ls)}  { ('-include ').join(self.sep_include_val_ls)} -c {self.srcFile}"
+        return as_clang_cmd_part
