d8f31b4ca6134ff57c66c8b920d4bba3fcc9adb0
prgrmz01@163.com <prgrmz01@163.com>
2023-12-05 09:15:21 +0800

    安装脚本

--- install-example.sh
+++ install-example.sh
@@ -4,6 +4,10 @@
 
 # CLANG_HOME_BIN=/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin
 
+#当前用户添加到sudo组，以免去使用sudo
+sudo usermod -aG sudo $(whomai)
+
+
 interceptor=/crk/cmd-wrap/interceptor.py
 
 fake_bin=/crk/bin
