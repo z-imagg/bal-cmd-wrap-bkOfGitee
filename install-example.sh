d84e82105c2cab5f8fea6f0af389d895656d85b1
prgrmz01@163.com <prgrmz01@163.com>
2023-12-05 09:08:59 +0800

    安装脚本

--- install-example.sh
+++ install-example.sh
@@ -4,43 +4,49 @@
 
 # CLANG_HOME_BIN=/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin
 
-interceptor=/app/cmd_interceptor/interceptor.py
+interceptor=/crk/cmd-wrap/interceptor.py
 
-fuke_clang=/bin/clang
-fuke_clangPP=/bin/clang++
-fuke_gcc=/bin/gcc
-fuke_gpp=/bin/g++
+fake_bin=/crk/bin
+export PATH=$fake_bin:$PATH
+
+sudo mkdir -p $fake_bin
+
+fake_clang=$fake_bin/clang
+fake_clangPP=$fake_bin/clang++
+fake_gcc=$fake_bin/gcc
+fake_gpp=$fake_bin/g++
 
 #安装被拦截clang举例:
-ln -v -s $interceptor $fuke_clang
+ln -v -s $interceptor $fake_clang
 
 #安装被拦截clang++举例:
-ln -v -s $interceptor $fuke_clangPP
+ln -v -s $interceptor $fake_clangPP
 
 #安装被拦截gcc举例:
-ln -v -s $interceptor $fuke_gcc
+ln -v -s $interceptor $fake_gcc
 
 #安装被拦截g++举例:
-ln -v -s $interceptor $fuke_gpp
+ln -v -s $interceptor $fake_gpp
 
-#由于 /bin/clang  在 PATH 中 比 /usr/bin/gcc 更先被搜索到
 
-#请求 假的 /bin/clang 时 ，发生转发:
-#/bin/clang ---软连接---> interceptor.py -----由route_tab.py转发---->  $CLANG_HOME_BIN/clang
+#由于 /crk/bin/clang  在 PATH 中 比 /usr/bin/gcc 更先被搜索到
 
-#请求 假的 /bin/gcc 时 ，发生转发:
-#/bin/gcc ---软连接---> interceptor.py -----由route_tab.py转发---->  /usr/bin/gcc-4.4
+#请求 假的 /crk/bin/clang 时 ，发生转发:
+#/crk/bin/clang ---软连接---> interceptor.py -----由route_tab.py转发---->  $CLANG_HOME_BIN/clang
+
+#请求 假的 /crk/bin/gcc 时 ，发生转发:
+#/crk/bin/gcc ---软连接---> interceptor.py -----由route_tab.py转发---->  /usr/bin/gcc-4.4
 
 
 
 #卸载被拦截clang举例:
-unlink $fuke_clang
+#unlink $fake_clang
 
 #卸载被拦截clang++举例:
-unlink  $fuke_clangPP
+#unlink  $fake_clangPP
 
 #卸载被拦截gcc举例:
-unlink  $fuke_gcc
+#unlink  $fake_gcc
 
 #卸载被拦截g++举例:
-unlink  $fuke_gpp
+#unlink  $fake_gpp
