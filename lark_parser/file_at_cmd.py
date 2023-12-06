bdbf8316e69a947dd9847f992972c8ad2329d5ab
prgrmz01@163.com <prgrmz01@163.com>
2023-12-06 19:32:17 +0800

    dev

--- lark_parser/file_at_cmd.py
+++ lark_parser/file_at_cmd.py
@@ -3,10 +3,18 @@ from typing import List
 #在 ubuntu22上 将 ubuntu14的根目录 被 挂载 为 目录  /ubt14x86root
 class FileAtCmd:
     @staticmethod
-    def __ls_join__(ls:List[str],sep:str):
-        assert sep is not None
-        if ls is None or len(ls) == 0 : return None
-        long_text:str=sep.join(ls)
+    def __NoneStr2Empty__(string:str):
+        if string is None: return ''
+        return string
+    
+    Kv1SepKv2=" "
+    @staticmethod
+    def __ls_join__(ls:List[str], argNameSep:str):
+        assert argNameSep is not None
+        if ls is None or len(ls) == 0 : return ''
+        #数组ls前面加一个空串，不然join结果不对。
+        lsX=['',*ls]
+        long_text:str=f"{FileAtCmd.Kv1SepKv2}{argNameSep}".join(lsX)
         return long_text
 
     def __init__(self):
@@ -51,12 +59,12 @@ class FileAtCmd:
     def __as_clang_cmd_part__(self)->str:
 
         # -m32
-        _m_dd_val: str  = self.m_dd_val
+        _m_dd_val: str  = FileAtCmd.__NoneStr2Empty__(self.m_dd_val)
         # -march=yyy
-        _m_arch_val: str=self.m_arch_val
+        _m_arch_val: str= FileAtCmd.__NoneStr2Empty__(self.m_arch_val)
 
         # -std=yy
-        _std_val: str = self.std_val
+        _std_val: str =  FileAtCmd.__NoneStr2Empty__(self.std_val)
 
         # -Dxxx
         _d_val_ls:str  = FileAtCmd.__ls_join__(self.d_val_ls,'-D')
@@ -83,6 +91,6 @@ class FileAtCmd:
         _sep_include_val_ls:str=FileAtCmd.__ls_join__(self.sep_include_val_ls,'-include ')
         _srcFile:str=self.src_file
 
-        as_clang_cmd_part= f" {_m_dd_val} {_m_arch_val} {_std_val}  {_d_val_ls} {_d_eq_val_ls} {_w_val_ls} {_W_eq_val_ls} {_f_val_ls} {_f_eq_val_ls} {_isystem_val_ls} {_inc_val_ls}  {_sep_inc_val_ls}  { _sep_include_val_ls} -c {_srcFile}"
+        as_clang_cmd_part= f" -m{_m_dd_val} {_m_arch_val} {_std_val}  {_d_val_ls} {_d_eq_val_ls} {_w_val_ls} {_W_eq_val_ls} {_f_val_ls} {_f_eq_val_ls} {_isystem_val_ls} {_inc_val_ls}  {_sep_inc_val_ls}  { _sep_include_val_ls} -c {_srcFile}"
 
         return as_clang_cmd_part
