fc867ea658910def2a5737121bae431bef39e378
prgrmz01@163.com <prgrmz01@163.com>
2023-12-07 19:59:37 +0800

    草稿：python 用fcntl加锁日志文件，最多同时N个进程使用不同的日志文件

--- interceptor.py
+++ interceptor.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 #apt install file uuid-runtime
-
+import errno
 import sys
 assert sys.version_info >= (3,6), "错误：需要使用 Python 3.6 或更高版本. 因为 此脚本中大量使用的 字符串格式化语法 f'{变量名}' 是pytho3.6引入的"
 import time
 import subprocess
 from typing import List,Tuple
+import fcntl
+
 
 from lark_parser.file_at_cmd import FileAtCmd
 from route_tab import calcTrueProg
@@ -44,24 +46,57 @@ Argv:List[str] = ArgvRemoveWerror(Argv)
 Argv[0]=calcTrueProg(Argv[0])
 #生成唯一文件路径（ 保存命令内容的文件 OF_cmd 、保存命令标准输出的文件 OF_stdout、保存命令错误输出的文件 OF_stderr）
 OFPath_cmd, OFPath_stdout, OFPath_stderr = getOutFilePathLs(progFake)
-gLogF=open("/crk/g.log", "w")
-#日志不能打印到标准输出、错误输出，因为有些调用者假定了标准输出就是他想要的返回内容。
-print(f"Argv:{Argv}",file=gLogF)
-print( f"收到命令及参数: {_cmdReceived}",file=gLogF )
-#用lark解析单gcc命令 并取出 命令 中的 源文件、头文件目录列表
-fileAtCmd:FileAtCmd=larkGetSrcFileFromSingleGccCmd(_cmdReceived,gLogF)
-if fileAtCmd.src_file is not None: #当 命令中 有源文件名，才截此命令
-    #调用本主机ubuntu22x64上的clang插件修改本地源文件
-    clangAddFuncIdAsmWrap(fileAtCmd,gLogF)
-else:
-    print(f"此命令【{_cmdReceived}】中 无源文件名，不拦截此命令",file=gLogF)
-    
-#执行真命令(真gcc命令编译已经被clang-add-funcIdAsm修改过的源文件）
-exitCode:int=execute_cmd(Argv, OFPath_cmd, gLogF,fileAtCmd.input_is_std_in)
-#显示命令输出、退出代码（输出包括 标准输出、错误输出）
 
-#关闭日志文件
-gLogF.close()
-#以真实命令的退出码退出（假装自己是真实命令）
-exit(exitCode)
+#尝试锁定日志文件，最多尝试N次
+gLogF_LockOk:bool=False
+Max_Try_Lock_Times=100
+for k in range(Max_Try_Lock_Times):
+    try:
+        logFK=f"/crk/g-{k}.log"
+        gLogF = open(logFK, "w")
+        # 锁定文件的一部分
+        fcntl.flock(gLogF.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+        print(f"日志文件{logFK}锁定成功,立即退出循环")
+        #一旦成功锁定某个日志文件，即可退出循环
+        gLogF_LockOk=True
+        break
+
+    except IOError as e:
+        if e.errno == errno.EAGAIN or e.errno == errno.EACCES:
+            print(f"日志文件{logFK}锁定失败，异常【{e}】")
+        else:
+            print(f"日志文件{logFK}锁定失败，其他异常【{e}】")
+    finally:
+        if not gLogF_LockOk :#若没拿到锁，但文件已经打开，则要关闭文件
+            if gLogF is not None:
+                gLogF.close()
+                gLogF=None
+
+assert gLogF is not None,f"断言错误，尝试锁定{k}次不同日志文件，依然锁定失败(此时已经有{k}个进程同时需要独立的日志文件？)。 最后尝试日志文件是【{logFK}】。请检查代码，应该是bug。"
+
+try:#try业务块
+    #日志不能打印到标准输出、错误输出，因为有些调用者假定了标准输出就是他想要的返回内容。
+    print(f"Argv:{Argv}",file=gLogF)
+    print( f"收到命令及参数: {_cmdReceived}",file=gLogF )
+    #用lark解析单gcc命令 并取出 命令 中的 源文件、头文件目录列表
+    fileAtCmd:FileAtCmd=larkGetSrcFileFromSingleGccCmd(_cmdReceived,gLogF)
+    if fileAtCmd.src_file is not None: #当 命令中 有源文件名，才截此命令
+        #调用本主机ubuntu22x64上的clang插件修改本地源文件
+        clangAddFuncIdAsmWrap(fileAtCmd,gLogF)
+    else:
+        print(f"此命令【{_cmdReceived}】中 无源文件名，不拦截此命令",file=gLogF)
+
+    #执行真命令(真gcc命令编译已经被clang-add-funcIdAsm修改过的源文件）
+    exitCode:int=execute_cmd(Argv, OFPath_cmd, gLogF,fileAtCmd.input_is_std_in)
+    #显示命令输出、退出代码（输出包括 标准输出、错误输出）
+finally:
+    #不论以上 try业务块 发生什么异常，本finally块一定要执行。
+    #关闭日志文件
+    gLogF.close()
+    #释放日志文件锁，否则其他进程无法使用本次被锁定的日志文件。
+    fcntl.flock(gLogF.fileno(), fcntl.LOCK_UN)
+    print(f"已释放日志文件{logFK}锁")
+    #以真实命令的退出码退出（假装自己是真实命令）
+    exit(exitCode)
 #拦截过程 结束}
