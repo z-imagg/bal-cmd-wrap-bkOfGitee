439d12f4b0a8e719e2e71a5aa5266478b736dab4
prgrmz01@163.com <prgrmz01@163.com>
2023-12-08 15:03:58 +0800

    本python进程的标准输入 给到 真实命令进程 的 标准输入

--- interceptor_util.py
+++ interceptor_util.py
@@ -31,36 +31,27 @@ def execute_cmd(Argv, OFPath_cmd, gLogF,input_is_std_in:bool)->int:
     exitCode:int=None
     print(f"【Argv@execute_cmd】:【{Argv}】", file=gLogF)
     with open(OFPath_cmd, "w") as ofCmd:
-        ArgvStr:str=' '.join(Argv) ; #breakpoint() # ?断点不起作用? 可能是输入被怎么了？
         #命令内容写入文件，方便问题查找.
+        ArgvStr:str=' '.join(Argv)
         ofCmd.write(f"真实命令:{ArgvStr}\n")
-        #subprocess.run(popenargs,...有名字参数...), 第一个参数popenargs 即 待执行 命令内容 有以下两个形式：
-        #    1. 不填写shell参数 或 shell=False   ， 即 命令内容 样式为 [程序名,参数1,参数2,...,参数k,...]:List[str], 即 本函数内的变量 Argv
-        #    2. shell=True ，则 命令内容 样式为 '程序名 参数1 参数2 ... 参数k ...' : str, 本函数内无此变量
-        #这里使用的是形式1
         print("执行真实命令:",Argv,file=gLogF)
+
         # 调用真实命令，
-        real_prog:plumbum.machines.local.LocalCommand=local[Argv[0]]
-        if input_is_std_in:
-            #问题描述, 在 g-0.log 有 报错命令:
-            """
-            收到命令及参数: /crk/bin/i686-linux-gnu-gcc -D__ASSEMBLY__ -m64 -c -x assembler -o .tmp_36077/tmp -
-            ...
-            真实命令错误输出【{standard input}: Assembler messages:
-{standard input}:2: Error: bad register expression
-            """
-            #模拟该报错命令情况如下：
-            #      echo " nop " |  /usr/bin/i686-linux-gnu-gcc -D__ASSEMBLY__ -m64 -c -x assembler -o xxx.o  -
-            #简陋排查办法 如下:
-            #      echo " print( type(sys.stdin.buffer)) " |  /crk/bin/i686-linux-gnu-gcc -D__ASSEMBLY__ -m64 -c -x assembler -o xxx.o  -
-            #           可知 type(sys.stdin.buffer) 为 <class '_io.BufferedReader'>
-            print(f"标准输入为:【{sys.stdin.read()}】") #输入接管道时 加断点 调试
-            #参考: https://plumbum.readthedocs.io/en/latest/local_commands.html#pipelining
-            real_prog=(real_prog < sys.stdin)
-        argLs=Argv[1:] if len(Argv) > 1 else []
-        real_cmd:plumbum.commands.base.BoundCommand=real_prog[argLs]
         retCode: int; std_out: str; err_out: str
-        exitCode, std_out, err_out = real_cmd.run(retcode=None)
+        if input_is_std_in:
+            #本python进程的标准输入 给到 真实命令进程 的 标准输入
+            p:subprocess.Popen = subprocess.Popen(Argv, stdin=subprocess.PIPE)
+            stdin_str:str=sys.stdin.read()
+            stdin_bytes:bytes=stdin_str.decode()
+            std_out, err_out=p.communicate(input=stdin_bytes)
+            exitCode=p.returncode
+            print(f"标准输入为:【{stdin_str}】") #输入接管道时 加断点 调试
+        else:
+            real_prog:plumbum.machines.local.LocalCommand=local[Argv[0]]
+            argLs=Argv[1:] if len(Argv) > 1 else []
+            real_cmd:plumbum.commands.base.BoundCommand=real_prog[argLs]
+            exitCode, std_out, err_out = real_cmd.run(retcode=None)
+
         # 写 真实命令的 标准输出、错误输出  (不能写到文件，因为调用者可能需要这些输出）
         if not __NoneOrLenEq0__(std_out):
             print(std_out,file=sys.stdout,end="") #真实命令的输出，不要有多余的换行
