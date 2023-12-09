b26468d96a31ba2aef7c9907dd7a78130223d2dd
prgrmz01@163.com <prgrmz01@163.com>
2023-12-09 21:44:41 +0800

    fix: 重命名上一次的日志文件

--- install-wrap.sh
+++ install-wrap.sh
@@ -21,9 +21,9 @@ export PYTHONPATH=/crk/cmd-wrap/lark_parser/:$PYTHONPATH
 #' | tee -a $BashRcF ;}
 
 #重命名上一次的日志文件
-InteceptorLogF=/crk/g-0.log
-UniqueId=$(date +"%Y%m%d%H%M%S_%s_%N")
-[ -f $InteceptorLogF ] && mv $InteceptorLogF "$InteceptorLogF_$UniqueId"
+InterceptorLogF=/crk/g-0.log
+UniqueId="$InterceptorLogF-$(date +'%Y%m%d%H%M%S_%s_%N')"
+[ -f $InterceptorLogF ] && mv $InterceptorLogF "$InterceptorLogF_$UniqueId"
 
 sudo mkdir -p $fake_bin && sudo chown -R $(id -gn).$(whoami) $fake_bin
 
