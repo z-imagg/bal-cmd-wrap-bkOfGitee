6883a6cfe25337b3248975b2cbe5ac8efb5ac4a7
prgrmz01@163.com <prgrmz01@163.com>
2023-12-09 12:06:45 +0800

    gcc_cmd_5: 源文件在中间，末尾依然是选项

--- lark_parser/lark_develop.py
+++ lark_parser/lark_develop.py
@@ -14,7 +14,8 @@ from lark_my_transformer import MyTransformer
 # gcc_cmd_line="  gcc -nostdlib -o arch/x86/vdso/vdso32-int80.so.dbg -fPIC -shared  -Wl,--hash-style=sysv -m32 -Wl,-soname=linux-gate.so.1 -Wl,-T,arch/x86/vdso/vdso32/vdso32.lds arch/x86/vdso/vdso32/note.o arch/x86/vdso/vdso32/int80.o"
 from file_at_cmd import FileAtCmd
 
-gcc_cmd_line='/crk/bin/i686-linux-gnu-gcc -E -x c -'
+#gcc_cmd_5: 源文件在中间，末尾依然是选项
+gcc_cmd_line='i686-linux-gnu-gcc -Werror -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -mretpoline-external-thunk -c -x c /dev/null -o .tmp_28820/tmp'
 #此命令输出是: /usr/lib/gcc-cross/i686-linux-gnu/11/include
 #此命令 应该是 make工具为了探测 给出的，并不是编译业务命令
 
@@ -41,16 +42,77 @@ end=True
 D:\miniconda3\python.exe F:/crk/bochs/cmd-wrap/lark_parser/lark_develop.py
 start
   gcc_cmd
-    gcc_cmd_2
-      program	/crk/bin/i686-linux-gnu-gcc
+    gcc_cmd_5
+      program	i686-linux-gnu-gcc
       kv_ls
         kv
-          kv_k_eq_valnorm
-            key	-print-file-name
+          kv_w__valany
+            -W
+            w_val	error
+        kv
+          kv_d__valany
+            -D
+            d_val	__KERNEL__
+        kv
+          kv_w__valany
+            -W
+            w_val	all
+        kv
+          kv_w__valany
+            -W
+            w_val	undef
+        kv
+          kv_w__valany
+            -W
+            w_val	strict-prototypes
+        kv
+          kv_w__valany
+            -W
+            w_val	no-trigraphs
+        kv
+          kv_f__valany
+            -f
+            f_val	no-strict-aliasing
+        kv
+          kv_f__valany
+            -f
+            f_val	no-common
+        kv
+          kv_f__valany
+            -f
+            f_val	short-wchar
+        kv
+          kv_w__valany
+            -W
+            w_val	error-implicit-function-declaration
+        kv
+          kv_w__valany
+            -W
+            w_val	no-format-security
+        kv
+          kv_std_eq_valany
+            -std
             sep_eq
-            include
+            std_val	gnu89
+        kv
+          key	-mretpoline-external-thunk
+        kv
+          key	-c
+        kv
+          kv_k_spc_valnorm
+            key	-x
+            sep_spc
+            c
+      src_file	/dev/null
+      kv_ls
+        kv
+          kv_k_spc_valnorm
+            key	-o
+            sep_spc
+            .tmp_28820/tmp
 
-此命令中 无源文件名，不拦截此命令
+命令中的源文件相关字段为:   -std=gnu89  -D__KERNEL__  -Werror -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -Werror-implicit-function-declaration -Wno-format-security  -fno-strict-aliasing -fno-common -fshort-wchar        -c /dev/null
+
 
 
 
