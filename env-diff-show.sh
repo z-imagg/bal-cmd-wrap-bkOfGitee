68fba870560c0dc5e8c7ce788e189425b1c5338a
prgrmz01@163.com <prgrmz01@163.com>
2023-12-11 10:56:27 +0800

    捕捉编译时的env环境变量和初始环境变量差异(写文件)

--- env-diff-show.sh
+++ env-diff-show.sh
@@ -2,7 +2,10 @@
 
 ignore_env_name_list_f=/crk/.ignore_env_name_list.txt
 { [ -f $ignore_env_name_list_f ] || exit 1  ;} && \
+UniqueId="$fileName-$(date +'%Y%m%d%H%M%S_%s_%N')" && \
+ResultF="/tmp/env-diff-$UniqueId.txt" && \
 ignoreNameLs=$(cat  $ignore_env_name_list_f | xargs -I% echo -n "-u % ") && \
 #env -u key1 -u key2 ... command
-env $ignoreNameLs
+env $ignoreNameLs > $ResultF && \
+echo $ResultF
 
