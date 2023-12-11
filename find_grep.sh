add12e202582227f103c932536a5631f4929dad5
prgrmz01@163.com <prgrmz01@163.com>
2023-12-12 03:01:27 +0800

    find_grep.sh

---
+++ find_grep.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+word=$1
+
+#find . -not -path "./.git/*"   -name "*.h" | xargs -I% sh -c 'grep -Hn   CR3_ADDR_MASK  % | egrep "#[[:space:]]*define CR3_ADDR_MASK|typedef[[:space:]]+CR3_ADDR_MASK[[:space:]]*"  '
+
+find . -not -path "./.git/*"   -name "*.h" | xargs -I% sh -c 'grep -Hn   $word  % | egrep "#[[:space:]]*define ${word}|typedef[[:space:]]+${word}[[:space:]]*"  '
