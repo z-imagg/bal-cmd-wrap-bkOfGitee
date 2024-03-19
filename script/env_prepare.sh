de5d429b9bec5d1d8551776f6aaeb11c8fd3b2ec
prgrmz07 <prgrmz07@163.com>
2024-03-19 15:43:39 +0800

    脚本

--- script/env_prepare.sh
+++ script/env_prepare.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-#source me.sh 或 bash me.sh 均能获取当前脚本完整路径的写法
+shopt -s expand_aliases
 alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
-#####
-D=${d}
-cd ${D}
-Hm="${D}/../" # == /fridaAnlzAp/cmd-wrap/
-
-
-VENV_HOME=${Hm}/.venv
-ActivVenv=$VENV_HOME/bin/activate
-test -f $ActivVenv || python3 -m venv $VENV_HOME
-
-
-source $ActivVenv
-pip install -r ${Hm}/requirements.txt
-
+getCurScriptFullPath
