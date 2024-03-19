ef30595cb08d6b615b238af75cd11001e3d1af1f
prgrmz07 <prgrmz07@163.com>
2024-03-19 15:44:15 +0800

    脚本

--- script/env_prepare.sh
+++ script/env_prepare.sh
@@ -3,3 +3,18 @@
 shopt -s expand_aliases
 alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
 getCurScriptFullPath
+
+#####
+
+D=${d}
+cd ${D}
+Hm="${D}/../" # == /fridaAnlzAp/cmd-wrap/
+
+
+VENV_HOME=${Hm}/.venv
+ActivVenv=$VENV_HOME/bin/activate
+test -f $ActivVenv || python3 -m venv $VENV_HOME
+
+
+source $ActivVenv
+pip install -r ${Hm}/requirements.txt
