7e5e4ee5e0cbd6044fc21b5fe8c6dae7581c6ca1
prgrmz07 <prgrmz07@163.com>
2024-03-19 16:08:03 +0800

    草稿中

--- script/env_prepare.sh
+++ script/env_prepare.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-#source me.sh 或 bash me.sh 均能获取当前脚本完整路径的写法
+#【文件作用】 整个项目的环境准备
+
+
+# 获取当前脚本完整路径的写法
+#   若将以下这段脚本 写如文件f.sh , 
+#       则 在调用者脚本中 书写 " source f.sh ( 或 bash f.sh  ) ; getCurScriptFullPath " 即可获得调用者脚本的完整路径
 shopt -s expand_aliases
 alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
-#取当前脚本完整路径的写法
+
+#取当前脚本完整路径
 getCurScriptFullPath
 #d==/fridaAnlzAp/cmd-wrap/script/
 
