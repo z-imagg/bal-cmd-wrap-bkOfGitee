cfbd4e38833793660d5168476925ab9cb3f91fa6
prgrmz07 <prgrmz07@163.com>
2024-03-20 16:36:31 +0800

    trueProg--->buszProg

--- py_util/route_tab.py
+++ py_util/route_tab.py
@@ -24,18 +24,18 @@ fake_gxx=pathNorm(f"{fakeBinHm}/g++")
 fack_cxx="/usr/bin/c++"
 
 LLVM15Home="/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4"
-true_clang=pathNorm(f"{LLVM15Home}/bin/clang")
-true_clangxx=pathNorm(f"{LLVM15Home}/bin/clang++")
-true_gcc=pathNorm(f"/usr/bin/x86_64-linux-gnu-gcc-11")
-# true_gxx=pathNorm(f"/usr/bin/x86_64-linux-gnu-g++-11")
-true_cxx=pathNorm(f"/usr/bin/c++.origin") #  $(readlink -f /usr/bin/c++.origin) ==  /usr/bin/x86_64-linux-gnu-g++-11
+busz_clang=pathNorm(f"{LLVM15Home}/bin/clang")
+busz_clangxx=pathNorm(f"{LLVM15Home}/bin/clang++")
+busz_gcc=pathNorm(f"/usr/bin/x86_64-linux-gnu-gcc-11")
+# busz_gxx=pathNorm(f"/usr/bin/x86_64-linux-gnu-g++-11")
+busz_cxx=pathNorm(f"/usr/bin/c++.origin") #  $(readlink -f /usr/bin/c++.origin) ==  /usr/bin/x86_64-linux-gnu-g++-11
 
 progTab=[
-(fake_clang,Prog(fake_clang,true_clang,Prog.ProgKind.Compiler) ),
-(fake_clangxx,Prog(fake_clangxx,true_clangxx,Prog.ProgKind.Compiler) ),
-(fake_gcc,Prog(fake_gcc,true_gcc,Prog.ProgKind.Compiler) ),
-# (fake_gxx,Prog(fake_gxx,true_cxx,Prog.ProgKind.Compiler) ),
-(fack_cxx,Prog(fack_cxx,true_cxx,Prog.ProgKind.Compiler) ),
+(fake_clang, Prog(fake_clang,busz_clang,Prog.ProgKind.Compiler) ),
+(fake_clangxx, Prog(fake_clangxx,busz_clangxx,Prog.ProgKind.Compiler) ),
+(fake_gcc, Prog(fake_gcc,busz_gcc,Prog.ProgKind.Compiler) ),
+# (fake_gxx, Prog(fake_gxx,true_cxx,Prog.ProgKind.Compiler) ),
+(fack_cxx, Prog(fack_cxx,busz_cxx,Prog.ProgKind.Compiler) ),
 ]
 
 progMap:typing.Dict[str,Prog]=dict(progTab)
