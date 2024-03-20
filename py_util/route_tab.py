68290ee35f5a495a01f66b942088bfaa654470de
prgrmz07 <prgrmz07@163.com>
2024-03-21 00:58:18 +0800

    cmd_setup.sh 草稿中

--- py_util/route_tab.py
+++ py_util/route_tab.py
@@ -20,28 +20,28 @@ progMap:typing.Dict[str,Prog]=dict()
 
 
 #入口者 家
-fakeBinHm="/fridaAnlzAp/cmd-wrap/bin/"
+fakeBinHm="/usr/bin/"
 
 #clang系列编译器 家
 LLVM15Home="/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4"
 
 #编译器 拦截： 入口者 --> 业务者
-fake_clang=pathNorm(f"{fakeBinHm}/clang") #编译器 入口者
+fake_clang=pathNorm(f"/usr/bin/clang") #编译器 入口者
 busz_clang=pathNorm(f"{LLVM15Home}/bin/clang") #编译器 业务者
 progMap[fake_clang]   = Prog(fake_clang,busz_clang,Prog.ProgKind.Compiler) # 入口者 --> 业务者
 
-fake_clangxx=pathNorm(f"{fakeBinHm}/clang++") #编译器 入口者
+fake_clangxx=pathNorm(f"/usr/bin/clang++") #编译器 入口者
 busz_clangxx=pathNorm(f"{LLVM15Home}/bin/clang++") #编译器 业务者
 progMap[fake_clangxx] = Prog(fake_clangxx,busz_clangxx,Prog.ProgKind.Compiler) # 入口者 --> 业务者
 
-fake_gcc=pathNorm(f"{fakeBinHm}/gcc") #编译器 入口者
-busz_gcc=pathNorm(f"/usr/bin/x86_64-linux-gnu-gcc-11") #编译器 业务者
+fake_gcc=pathNorm(f"/usr/bin/gcc") #编译器 入口者
+busz_gcc=pathNorm(f"/usr/bin/gcc.origin") #编译器 业务者
 progMap[fake_gcc]     = Prog(fake_gcc,busz_gcc,Prog.ProgKind.Compiler) # 入口者 --> 业务者
 
-# 这组未测试
-# fake_gxx=pathNorm(f"{fakeBinHm}/g++") #编译器 入口者
-# busz_gxx=pathNorm(f"/usr/bin/g++.origin") #编译器 业务者
-# progMap[fake_gxx]   = Prog(fake_gxx,busz_gxx,Prog.ProgKind.Compiler)
+# 这g++组未测试
+fake_gxx=pathNorm(f"/usr/bin/g++") #编译器 入口者
+busz_gxx=pathNorm(f"/usr/bin/g++.origin") #编译器 业务者
+progMap[fake_gxx]   = Prog(fake_gxx,busz_gxx,Prog.ProgKind.Compiler)
 
 fake_cxx="/usr/bin/c++" #编译器 入口者
 busz_cxx=pathNorm(f"/usr/bin/c++.origin") #编译器 业务者  #  $(readlink -f /usr/bin/c++.origin) ==  /usr/bin/x86_64-linux-gnu-g++-11
