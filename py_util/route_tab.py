8e8dc65223cc5327c5dac6f3ab79817077df7fd9
prgrmz07 <prgrmz07@163.com>
2024-03-20 22:56:21 +0800

    【调整定义风格】使得更不容易出错、更容易阅读： 拦截者 --> 业务者

--- py_util/route_tab.py
+++ py_util/route_tab.py
@@ -18,39 +18,43 @@ class Prog:
 
 progMap:typing.Dict[str,Prog]=dict()
 
+
+#入口者 家
 fakeBinHm="/fridaAnlzAp/cmd-wrap/bin/"
-#编译器 拦截者
-fake_clang=pathNorm(f"{fakeBinHm}/clang")
-fake_clangxx=pathNorm(f"{fakeBinHm}/clang++")
-fake_gcc=pathNorm(f"{fakeBinHm}/gcc")
-fake_gxx=pathNorm(f"{fakeBinHm}/g++")
-fake_cxx="/usr/bin/c++"
-#构建工具 拦截者
-fake_cmake="/usr/bin/cmake"
-fake_make="/usr/bin/make"
 
+#clang系列编译器 家
 LLVM15Home="/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4"
-#编译器 业务者
-busz_clang=pathNorm(f"{LLVM15Home}/bin/clang")
-busz_clangxx=pathNorm(f"{LLVM15Home}/bin/clang++")
-busz_gcc=pathNorm(f"/usr/bin/x86_64-linux-gnu-gcc-11")
-# busz_gxx=pathNorm(f"/usr/bin/x86_64-linux-gnu-g++-11")
-busz_cxx=pathNorm(f"/usr/bin/c++.origin") #  $(readlink -f /usr/bin/c++.origin) ==  /usr/bin/x86_64-linux-gnu-g++-11
-#构建工具 业务者
-busz_cmake=pathNorm(f"/usr/bin/cmake.origin")
-busz_make=pathNorm(f"/usr/bin/make.origin")
 
-#编译器： 拦截者 --> 业务者
-progMap[fake_clang]   = Prog(fake_clang,busz_clang,Prog.ProgKind.Compiler)
-progMap[fake_clangxx] = Prog(fake_clangxx,busz_clangxx,Prog.ProgKind.Compiler)
-progMap[fake_gcc]     = Prog(fake_gcc,busz_gcc,Prog.ProgKind.Compiler)
-# progMap[fake_gxx]   = Prog(fake_gxx,true_cxx,Prog.ProgKind.Compiler)
-progMap[fake_cxx]     = Prog(fake_cxx,busz_cxx,Prog.ProgKind.Compiler)
-#构建工具 拦截者 --> 业务者
-progMap[fake_cmake]   = Prog(fake_cmake,busz_cmake,Prog.ProgKind.MakeTool)
-progMap[fake_make]    = Prog(fake_make,busz_make,Prog.ProgKind.MakeTool)
+#编译器 拦截： 入口者 --> 业务者
+fake_clang=pathNorm(f"{fakeBinHm}/clang") #编译器 入口者
+busz_clang=pathNorm(f"{LLVM15Home}/bin/clang") #编译器 业务者
+progMap[fake_clang]   = Prog(fake_clang,busz_clang,Prog.ProgKind.Compiler) # 入口者 --> 业务者
 
+fake_clangxx=pathNorm(f"{fakeBinHm}/clang++") #编译器 入口者
+busz_clangxx=pathNorm(f"{LLVM15Home}/bin/clang++") #编译器 业务者
+progMap[fake_clangxx] = Prog(fake_clangxx,busz_clangxx,Prog.ProgKind.Compiler) # 入口者 --> 业务者
 
+fake_gcc=pathNorm(f"{fakeBinHm}/gcc") #编译器 入口者
+busz_gcc=pathNorm(f"/usr/bin/x86_64-linux-gnu-gcc-11") #编译器 业务者
+progMap[fake_gcc]     = Prog(fake_gcc,busz_gcc,Prog.ProgKind.Compiler) # 入口者 --> 业务者
+
+# 这组未测试
+# fake_gxx=pathNorm(f"{fakeBinHm}/g++") #编译器 入口者
+# busz_gxx=pathNorm(f"/usr/bin/g++.origin") #编译器 业务者
+# progMap[fake_gxx]   = Prog(fake_gxx,busz_gxx,Prog.ProgKind.Compiler)
+
+fake_cxx="/usr/bin/c++" #编译器 入口者
+busz_cxx=pathNorm(f"/usr/bin/c++.origin") #编译器 业务者  #  $(readlink -f /usr/bin/c++.origin) ==  /usr/bin/x86_64-linux-gnu-g++-11
+progMap[fake_cxx]     = Prog(fake_cxx,busz_cxx,Prog.ProgKind.Compiler) # 入口者 --> 业务者
+
+#构建工具 拦截： 入口者 --> 业务者
+fake_cmake="/usr/bin/cmake" #构建工具 入口者
+busz_cmake=pathNorm(f"/usr/bin/cmake.origin") #构建工具 业务者
+progMap[fake_cmake]   = Prog(fake_cmake,busz_cmake,Prog.ProgKind.MakeTool) # 入口者 --> 业务者
+
+fake_make="/usr/bin/make" #构建工具 入口者
+busz_make=pathNorm(f"/usr/bin/make.origin") #构建工具 业务者
+progMap[fake_make]    = Prog(fake_make,busz_make,Prog.ProgKind.MakeTool) # 入口者 --> 业务者
 
 def calcTrueProg( progAbsNormPath:str )->Prog:
     prgNmPth= progAbsNormPath
