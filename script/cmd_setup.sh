1c51f0eb960a3969abf41c6adddef8f0f3efdc81
prgrmz07 <prgrmz07@163.com>
2024-03-21 00:54:06 +0800

    cmd_setup.sh 草稿中

--- script/cmd_setup.sh
+++ script/cmd_setup.sh
@@ -25,18 +25,19 @@ getCurScriptFullPath
 declare -r interceptor_cxx="/fridaAnlzAp/cmd-wrap/bin/interceptor_cxx.py"
 chmod +x $interceptor_cxx
 
-alias _echoWhich='  _F=$( which ${_C} ) &&  echo -n " ${_C} -----> ${_F} "   '
-alias _echoReadlinkF_Ln='   echo  "  -----> $( readlink -f ${_F} ) "   '
+alias _1echoWhich='  _F=$( which ${_C} ) &&  echo -n " ${_C} -----> ${_F} "   '
+alias _2echoReadlinkF_Ln='   echo  "  -----> $( readlink -f ${_F} ) "   '
+alias _echoReadlinkF_Ln='   echo  " ${_F} -----> $( readlink -f ${_F} ) "   '
 #若是ELF文件，则备份
-alias _IfELFMvAsOrn='[[ "$( file --brief --mime-type ${Fil} )" == "application/x-pie-executable" ]] && { sudo mv -v "${Fil}" "${Fil}.origin.$(date +%s)"  && echo "是ELF文件 备份为$_"  ;}'
+alias _IfELFMvAsOrn='[[ "$( file --brief --mime-type ${Fil} )" == "application/x-pie-executable" ]] && { sudo mv  "${Fil}" "${Fil}.origin.$(date +%s)"  && echo "是ELF文件 备份为$_"  ;}'
 #若不是拦截入口者，则备份
-alias _IfNotItcpMvAsOrn='[[ $( readlink -f ${Fil} ) == "${interceptor_cxx}" ]] || { sudo mv -v "${Fil}" "${Fil}.origin.$(date +%s)"  && echo "非拦截入口 备份为$_" ;}'
+alias _IfNotItcpMvAsOrn='[[ $( readlink -f ${Fil} ) == "${interceptor_cxx}" ]] || { [[ -f ${Fil} ]] && sudo mv  "${Fil}" "${Fil}.origin.$(date +%s)"  && echo "非拦截入口 备份为$_" ;}'
 #销毁现有入口者
-alias _IfIntcptUnlnk='[[ $( readlink -f ${Fil} ) == "${interceptor_cxx}" ]] && sudo unlink "${Fil}" && echo "销毁现有入口者  ${Fil} ,  " '
+alias _IfIntcptUnlnk='{ echo -n "销毁现有入口者" &&  _F=${Fil} &&  _echoReadlinkF_Ln  ;} ; {  [[ $( readlink -f ${Fil} ) == "${interceptor_cxx}" ]] && sudo unlink "${Fil}"  ;} '
 #重新生成入口者
-alias _lnk2Intcpt='sudo ln -s "${interceptor_cxx}" "${Fil}" && echo "重新生成入口者 ${Fil}   " '
+alias _lnk2Intcpt='sudo ln -s "${interceptor_cxx}" "${Fil}" && echo -n "重新生成入口者  "  &&  _F=${Fil} &&  _echoReadlinkF_Ln  '
 #显示拦截器
-alias _echoLnk=' _C="${Cmd}" && echo -n "显示拦截器" &&  _echoWhich  && _echoReadlinkF_Ln   '
+alias _echoLnk=' _C="${Cmd}" && echo -n "显示拦截器" &&  _1echoWhich  && _2echoReadlinkF_Ln   '
 
 declare -r gccF="/usr/bin/gcc"
 Fil="${gccF}" ;  _IfELFMvAsOrn #备份
@@ -44,6 +45,7 @@ Fil="${gccF}" ; _IfNotItcpMvAsOrn #备份
 Fil="${gccF}" ; _IfIntcptUnlnk #销毁现有入口者
 Fil="${gccF}" ; _lnk2Intcpt #重新生成入口者
 Cmd="gcc"     ; _echoLnk #显示拦截器
+echo ""
 
 declare -r gxxF="/usr/bin/g++"
 Fil="${gxxF}" ;  _IfELFMvAsOrn #备份
@@ -51,6 +53,7 @@ Fil="${gxxF}" ; _IfNotItcpMvAsOrn
 Fil="${gxxF}" ; _IfIntcptUnlnk
 Fil="${gxxF}" ; _lnk2Intcpt
 Cmd="g++"     ; _echoLnk
+echo ""
 
 declare -r cxxF="/usr/bin/c++"
 Fil="${cxxF}" ;  _IfELFMvAsOrn #备份
@@ -58,6 +61,7 @@ Fil="${cxxF}" ; _IfNotItcpMvAsOrn
 Fil="${cxxF}" ; _IfIntcptUnlnk
 Fil="${cxxF}" ; _lnk2Intcpt
 Cmd="c++"     ; _echoLnk
+echo ""
 
 declare -r clangF="/usr/bin/clang"
 Fil="${clangF}" ;  _IfELFMvAsOrn #备份
@@ -65,6 +69,7 @@ Fil="${clangF}" ; _IfNotItcpMvAsOrn
 Fil="${clangF}" ; _IfIntcptUnlnk
 Fil="${clangF}" ; _lnk2Intcpt
 Cmd="clang"     ; _echoLnk
+echo ""
 
 declare -r clangxxF="/usr/bin/clang++"
 Fil="${clangxxF}" ;  _IfELFMvAsOrn #备份
@@ -72,6 +77,7 @@ Fil="${clangxxF}" ; _IfNotItcpMvAsOrn
 Fil="${clangxxF}" ; _IfIntcptUnlnk
 Fil="${clangxxF}" ; _lnk2Intcpt
 Cmd="clang++"     ; _echoLnk
+echo ""
 
 declare -r cmakeF="/usr/bin/cmake"
 Fil="${cmakeF}" ;  _IfELFMvAsOrn
@@ -79,6 +85,7 @@ Fil="${cmakeF}" ; _IfNotItcpMvAsOrn
 Fil="${cmakeF}" ; _IfIntcptUnlnk
 Fil="${cmakeF}" ; _lnk2Intcpt
 Cmd="cmake"     ; _echoLnk
+echo ""
 
 declare -r makeF="/usr/bin/make"
 Fil="${makeF}" ;  _IfELFMvAsOrn
@@ -86,6 +93,7 @@ Fil="${makeF}" ; _IfNotItcpMvAsOrn
 Fil="${makeF}" ; _IfIntcptUnlnk
 Fil="${makeF}" ; _lnk2Intcpt
 Cmd="make"     ; _echoLnk
+echo ""
 
 #测试拦截器化身(gcc)
 cd /tmp/
@@ -94,5 +102,5 @@ g++
 c++
 clang
 clang++
-cmake
-make
+cmake 2&>1 >/dev/null
+make  2&>1 >/dev/null
