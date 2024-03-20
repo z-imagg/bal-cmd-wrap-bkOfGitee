108365c45a6844ac9642262c6641eb8cce09b2f7
prgrmz07 <prgrmz07@163.com>
2024-03-20 12:32:05 +0800

    bash自动完成无意义，因为拦截器已经不允许运行自身了，只能以化身运行

--- script/cmd_setup.sh
+++ script/cmd_setup.sh
@@ -22,7 +22,6 @@ binHm=$Hm/bin
 intcpt=$binHm/interceptor_cxx.py
 #intcpt == /fridaAnlzAp/cmd-wrap/bin/interceptor_cxx.py
 
-source $Hm/script/bash-complete--interceptor_cxx.sh
 chmod +x $intcpt
 
 chmod +x $Hm/env-diff-show.sh
@@ -33,19 +32,24 @@ bash $Hm/script/env_prepare.sh >/dev/null
 source $Hm/.venv/bin/activate
 # set -x
 
+#生成 拦截器化身
+echo "清理 拦截器化身"
 unlink $binHm/gcc
 unlink $binHm/g++
 unlink $binHm/clang
 unlink $binHm/clang++
 
+echo "重新生成 拦截器化身"
 ln -s  $intcpt $binHm/gcc
 #ln -s /fridaAnlzAp/cmd-wrap/bin/interceptor_cxx.py   /fridaAnlzAp/cmd-wrap/bin/gcc
 ln -s  $intcpt $binHm/g++
 ln -s  $intcpt $binHm/clang
 ln -s  $intcpt $binHm/clang++
 
+echo "将 拦截器化身 放入 PATH 环境变量 中"
 export PATH=$binHm:$PATH
 
+echo "列出 拦截器化身 们"
 which gcc
 # /fridaAnlzAp/cmd-wrap/bin/gcc
 which g++
@@ -55,10 +59,16 @@ which clang
 which clang++
 # /fridaAnlzAp/cmd-wrap/bin/clang++
 
-#测试gcc拦截
-echo "测试gcc拦截"
+#测试拦截器化身(gcc)
+rm -frv /tmp/gcc-*.log
+
+echo "测试拦截器化身(gcc)开发者模式"
 gcc --__enable_develop_mode
 echo "请您用人类肉眼，确认gcc拦截器返回代码【$?】应该和上面拦截器日志中说的gcc命令的返回代码一致才对"
 
+
+echo "先用tail后台显示拦截器日志文件， 测试拦截器化身(gcc)安静模式"
+tail -f /tmp/gcc-*.log &
+gcc
 
 #interceptor_cxx.py --__help  及其 bash自动完成
