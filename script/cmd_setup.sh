2ee7935816764e6c2e369ab508534283b8349d68
prgrmz07 <prgrmz07@163.com>
2024-03-21 00:17:17 +0800

    cmd_setup.sh 草稿中

--- script/cmd_setup.sh
+++ script/cmd_setup.sh
@@ -9,86 +9,81 @@
 shopt -s expand_aliases
 alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
 
-
 #取当前脚本完整路径
 getCurScriptFullPath
 #d==/fridaAnlzAp/cmd-wrap/script/
 
-Hm=$(realpath -s ${d}/../)
-#Hm=/fridaAnlzAp/cmd-wrap
+######脚本正文开始
 
-binHm=$Hm/bin
-#binHm == /fridaAnlzAp/cmd-wrap/bin/
-intcpt=$binHm/interceptor_cxx.py
-#intcpt == /fridaAnlzAp/cmd-wrap/bin/interceptor_cxx.py
-
-chmod +x $intcpt
-
-bash $Hm/script/env_prepare.sh >/dev/null
+bash /fridaAnlzAp/cmd-wrap/script/env_prepare.sh >/dev/null
 
 # set +x
-source $Hm/.venv/bin/activate
+source /fridaAnlzAp/cmd-wrap/.venv/bin/activate
 # set -x
 
 #拦截器
 declare -r interceptor_cxx="/fridaAnlzAp/cmd-wrap/bin/interceptor_cxx.py"
+chmod +x $interceptor_cxx
 
 #若是ELF文件，则备份
 alias _IfELFMvAsOrn='[[ "$( file --brief --mime-type ${Fil} )" == "application/x-pie-executable" ]] && { sudo mv -v "${Fil}" "${Fil}.origin.$(date +%s)"  && echo "是ELF文件 备份为$_"  ;}'
-
 #若不是拦截入口者，则备份
 alias _IfNotItcpMvAsOrn='[[ $( readlink -f ${Fil} ) == "${interceptor_cxx}" ]] || { sudo mv -v "${Fil}" "${Fil}.origin.$(date +%s)"  && echo "非拦截入口 备份为$_" ;}'
-
 #销毁现有入口者
 alias _IfIntcptUnlnk='[[ $( readlink -f ${Fil} ) == "${interceptor_cxx}" ]] && sudo unlink "${Fil}" && echo "销毁现有入口者 $_" '
-
 #重新生成入口者
 alias _lnk2Intcpt='sudo ln -s "${interceptor_cxx}" "${Fil}" && echo "重新生成入口者 ${Fil}   " '
-
 #显示拦截器
 alias _echoLnk=' _cmd=$(which "${Cmd}") && echo "显示拦截器 ${Cmd} --> $(_cmd) " '
 
-Fil="/usr/bin/gcc" ;  _IfELFMvAsOrn #备份
-Fil="/usr/bin/gcc" ; _IfNotItcpMvAsOrn #备份
-Fil="/usr/bin/gcc" ; _IfIntcptUnlnk #销毁现有入口者
-Fil="/usr/bin/gcc" ; _lnk2Intcpt #重新生成入口者
-Cmd="gcc"          ; _echoLnk #显示拦截器
+declare -r gccF="/usr/bin/gcc"
+Fil="${gccF}" ;  _IfELFMvAsOrn #备份
+Fil="${gccF}" ; _IfNotItcpMvAsOrn #备份
+Fil="${gccF}" ; _IfIntcptUnlnk #销毁现有入口者
+Fil="${gccF}" ; _lnk2Intcpt #重新生成入口者
+Cmd="gcc"     ; _echoLnk #显示拦截器
 
-Fil="/usr/bin/g++" ;  _IfELFMvAsOrn #备份
-Fil="/usr/bin/g++" ; _IfNotItcpMvAsOrn
-Fil="/usr/bin/g++" ; _IfIntcptUnlnk
-Fil="/usr/bin/g++" ; _lnk2Intcpt
-Cmd="g++" ; _echoLnk
+declare -r gxxF="/usr/bin/g++"
+Fil="${gxxF}" ;  _IfELFMvAsOrn #备份
+Fil="${gxxF}" ; _IfNotItcpMvAsOrn
+Fil="${gxxF}" ; _IfIntcptUnlnk
+Fil="${gxxF}" ; _lnk2Intcpt
+Cmd="g++"     ; _echoLnk
 
-Fil="/usr/bin/c++" ;  _IfELFMvAsOrn #备份
-Fil="/usr/bin/c++" ; _IfNotItcpMvAsOrn
-Fil="/usr/bin/c++" ; _IfIntcptUnlnk
-Fil="/usr/bin/c++" ; _lnk2Intcpt
-Cmd="c++" ; _echoLnk
+declare -r cxxF="/usr/bin/c++"
+Fil="${cxxF}" ;  _IfELFMvAsOrn #备份
+Fil="${cxxF}" ; _IfNotItcpMvAsOrn
+Fil="${cxxF}" ; _IfIntcptUnlnk
+Fil="${cxxF}" ; _lnk2Intcpt
+Cmd="c++"     ; _echoLnk
 
-Fil="/usr/bin/clang" ;  _IfELFMvAsOrn #备份
-Fil="/usr/bin/clang" ; _IfNotItcpMvAsOrn
-Fil="/usr/bin/clang" ; _IfIntcptUnlnk
-Fil="/usr/bin/clang" ; _lnk2Intcpt
-Cmd="clang" ; _echoLnk
+declare -r clangF="/usr/bin/clang"
+Fil="${clangF}" ;  _IfELFMvAsOrn #备份
+Fil="${clangF}" ; _IfNotItcpMvAsOrn
+Fil="${clangF}" ; _IfIntcptUnlnk
+Fil="${clangF}" ; _lnk2Intcpt
+Cmd="clang"     ; _echoLnk
 
-Fil="/usr/bin/clang++" ;  _IfELFMvAsOrn #备份
-Fil="/usr/bin/clang++" ; _IfNotItcpMvAsOrn
-Fil="/usr/bin/clang++" ; _IfIntcptUnlnk
-Fil="/usr/bin/clang++" ; _lnk2Intcpt
-Cmd="clang++" ; _echoLnk
+declare -r clangxxF="/usr/bin/clang++"
+Fil="${clangxxF}" ;  _IfELFMvAsOrn #备份
+Fil="${clangxxF}" ; _IfNotItcpMvAsOrn
+Fil="${clangxxF}" ; _IfIntcptUnlnk
+Fil="${clangxxF}" ; _lnk2Intcpt
+Cmd="clang++"     ; _echoLnk
 
-Fil="/usr/bin/cmake" ;  _IfELFMvAsOrn
-Fil="/usr/bin/cmake" ; _IfNotItcpMvAsOrn
-Fil="/usr/bin/cmake" ; _IfIntcptUnlnk
-Fil="/usr/bin/cmake" ; _lnk2Intcpt
-Cmd="cmake" ; _echoLnk
+declare -r cmakeF="/usr/bin/cmake"
+Fil="${cmakeF}" ;  _IfELFMvAsOrn
+Fil="${cmakeF}" ; _IfNotItcpMvAsOrn
+Fil="${cmakeF}" ; _IfIntcptUnlnk
+Fil="${cmakeF}" ; _lnk2Intcpt
+Cmd="cmake"     ; _echoLnk
 
-Fil="/usr/bin/make" ;  _IfELFMvAsOrn
-Fil="/usr/bin/make" ; _IfNotItcpMvAsOrn
-Fil="/usr/bin/make" ; _IfIntcptUnlnk
-Fil="/usr/bin/make" ; _lnk2Intcpt
-Cmd="make" ; _echoLnk
+declare -r makeF="/usr/bin/make"
+Fil="${makeF}" ;  _IfELFMvAsOrn
+Fil="${makeF}" ; _IfNotItcpMvAsOrn
+Fil="${makeF}" ; _IfIntcptUnlnk
+Fil="${makeF}" ; _lnk2Intcpt
+Cmd="make"     ; _echoLnk
 
 #测试拦截器化身(gcc)
 cd /tmp/
