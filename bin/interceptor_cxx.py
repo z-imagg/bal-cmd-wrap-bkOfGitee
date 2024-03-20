5a29a512bef47e25673a4bcfa1d508377b7a6918
prgrmz07 <prgrmz07@163.com>
2024-03-20 14:54:04 +0800

    禁用开发代码【复制整个当前目录】

--- bin/interceptor_cxx.py
+++ bin/interceptor_cxx.py
@@ -48,8 +48,8 @@ GlbVar( )
 
 inst=getGlbVarInst()
 #开发用，复制整个当前目录，为了应对 cmake编译完删除临时问题的 行为，出错时候 已经找不到被编译文件了
-initCurDir4Deve= f"/tmp/{inst.initCurDir.replace('/','-')}"
-shutil.copytree(inst.initCurDir, initCurDir4Deve)
+# initCurDir4Deve= f"/tmp/{inst.initCurDir.replace('/','-')}"
+# shutil.copytree(inst.initCurDir, initCurDir4Deve)
 
 #{拦截过程 开始
 curFrm:types.FrameType=inspect.currentframe()
