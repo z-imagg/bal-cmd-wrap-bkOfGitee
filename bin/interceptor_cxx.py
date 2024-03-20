41e4aed212773eb0aa0fc9c0371fa23cafb4c6b6
prgrmz07 <prgrmz07@163.com>
2024-03-20 13:48:10 +0800

    草稿，修bug中

--- bin/interceptor_cxx.py
+++ bin/interceptor_cxx.py
@@ -78,12 +78,16 @@ except BaseException  as bexp:
     # raise bexp
 finally:
     #不论以上 try业务块 发生什么异常，本finally块一定要执行。
-    #立即 将 stdio缓存 写出 ， 关闭日志文件
-    flushStdCloseLogF()
+
     if exitCode != 0 :
         #如果异常退出，则以软链接指向日志文件，方便排查错误
         logFPth:str=getGlbVarInst().logFPth
-        Path(logFPth).link_to(f"errorCode_{exitCode}--{logFPth}")
+        Path(logFPth).link_to(f"{logFPth}--errorCode_{exitCode}")
+        
+    #立即 将 stdio缓存 写出 ， 关闭日志文件
+    flushStdCloseLogF()
+
+    #以业务命令的退出代码退出
     exit(exitCode)
 
 #拦截过程 结束}
