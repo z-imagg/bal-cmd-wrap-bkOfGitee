7ff0d51410f6482a9622be76a4714be130e289cc
prgrmz07 <prgrmz07@163.com>
2024-05-20 00:34:31 +0800

    输入材料 TestC02.c 变更

--- test_cxx_src/TestC02.c
+++ test_cxx_src/TestC02.c
@@ -1,3 +1,12 @@
+void voidEmptyFunc01(double* ptr_d, short num){
+
+}
+
+void voidRegularFunc01(double* ptr_d, short num){
+    if(num){
+        *ptr_d=0.9;
+    }
+}
 int main(int argc,char** argv){
     int num;
     int * ptr1=&num;
