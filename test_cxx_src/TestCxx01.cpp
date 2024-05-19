d27a82e94a2ab06a95d609f4783c220a675886ec
prgrmz07 <prgrmz07@163.com>
2024-05-20 00:52:25 +0800

    输入材料 TestCxx01.c 变更

--- test_cxx_src/TestCxx01.cpp
+++ test_cxx_src/TestCxx01.cpp
@@ -2,14 +2,21 @@ class MyUser{
 private :
 int id;
 public:
+MyUser(){
+
+}
+~MyUser(){
+    
+}
 int getId();
+
 };
 
 int MyUser::getId(){
     return this->id;
 }
 
-int main(char** argv, int argc){
+int main(int argc, char** argv){
     MyUser user1;
     MyUser* ptr1= new MyUser();
     return 0;
