7b7d4e094cc218263779cc9fe83209f462b270fd
prgrmz07 <prgrmz07@163.com>
2024-05-19 23:13:30 +0800

    [2] config修改, 确保cfg/_config_.py不能被外部直接导入，而只能通过cfg/__init__.py导入

--- cfg/_config_.py
+++ cfg/_config_.py
@@ -6,14 +6,16 @@
 
 import typing,types
 
+#  确保只能通过 'cfg/__init__.py' 导入 本文'cfg/_config_.py'
 import inspect
 _callStack:typing.List[types.FrameType]=inspect.stack()
+#   不关心调用栈中非本项目的'*.py'
 callStack:typing.List[types.FrameType]=list(filter(lambda s:s.filename  .startswith("/app/cmd-wrap/"), _callStack))
-assert callStack.__len__() >= 2
+assert callStack.__len__() >= 2, f"断言失败， 确保只能通过 'cfg/__init__.py' 导入 本文'cfg/_config_.py' . [1] . callStack=[{callStack}]"
 caller:types.FrameType=callStack[1]
 print(f"callStack=={callStack}")
-# print(f"caller=={caller}")
-assert caller.filename=='/app/cmd-wrap/cfg/__init__.py'
+# 确保只能通过 'cfg/__init__.py' 导入 本文'cfg/_config_.py'
+assert caller.filename=='/app/cmd-wrap/cfg/__init__.py', f"断言失败， 确保只能通过 'cfg/__init__.py' 导入 本文'cfg/_config_.py' . [2] . caller.filename=[{caller.filename}]"
 
 from cfg.config_base import OptName,OptModify,optModifyLs2Dict
 
