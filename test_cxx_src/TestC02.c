59c21f44d2fa07de86ff70dfb9d85237dae7e1c3
prgrmz07 <prgrmz07@163.com>
2024-05-19 16:44:30 +0800

    vscode运行配置

---
+++ test_cxx_src/TestC02.c
@@ -0,0 +1,5 @@
+int main(char** argv, int argc){
+    int num;
+    int ptr1=&num;
+    return 0;
+}
--- test_cxx_src/TestCxx01.cpp
+++ test_cxx_src/TestCxx01.cpp
@@ -8,3 +8,9 @@ int getId();
 int MyUser::getId(){
     return this->id;
 }
+
+int main(char** argv, int argc){
+    MyUser user1;
+    MyUser* ptr1= new MyUser();
+    return 0;
+}
