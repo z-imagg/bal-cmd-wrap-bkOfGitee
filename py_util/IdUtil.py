dec65b1a870343f2722fec10200f630fb2e8d5b8
prgrmz07 <prgrmz07@163.com>
2024-05-20 15:09:23 +0800

    python日期时间三方库pendulum： genApproxId加入人类可读时刻

--- py_util/IdUtil.py
+++ py_util/IdUtil.py
@@ -1,11 +1,17 @@
 
 
 import os
-import time
-
+import pendulum
+from pendulum.datetime import DateTime
+from pendulum.tz.timezone import Timezone
+    
 #生成近似唯一编号
 def genApproxId()->str:
     pid:int=os.getpid()
-    timeNs:int=time.time_ns()
-    id:str= f"{timeNs}-{pid}"
+    tz = Timezone('Asia/Shanghai')
+    now:DateTime=pendulum.now(tz)
+    now_human:str = now.strftime('%Y%m%d%H%M%S.%f')
+    # import time ; timeNs:int=time.time_ns()
+    timeNs:int=int(now.timestamp()*1e9)
+    id:str= f"{timeNs}-{now_human}-{pid}"
     return id
